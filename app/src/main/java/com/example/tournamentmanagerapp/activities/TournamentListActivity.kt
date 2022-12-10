e1f9161eb54d5380e83b9a7b0d940250f925a890
11xnt <allenterescencobq@gmail.com>
2022-12-10 23:23:15 +0000

    refactored tournament adapter into its own package

--- app/src/main/java/com/example/tournamentmanagerapp/activities/TournamentListActivity.kt
+++ app/src/main/java/com/example/tournamentmanagerapp/activities/TournamentListActivity.kt
@@ -4,18 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.tournamentmanagerapp.R
 import com.example.tournamentmanagerapp.databinding.ActivityTournamentListBinding
-import com.example.tournamentmanagerapp.databinding.CardTournamentBinding
 import com.example.tournamentmanagerapp.main.MainApp
-import com.example.tournamentmanagerapp.models.TournamentModel
+import com.example.tournamentmanagerapp.adapters.TournamentAdapter
 
 class TournamentListActivity : AppCompatActivity() {
 
@@ -60,34 +56,4 @@ class TournamentListActivity : AppCompatActivity() {
                 notifyItemRangeChanged(0,app.tournaments.size)
             }
         }
-}
-
-class TournamentAdapter constructor(private var tournaments: List<TournamentModel>) :
-
-    RecyclerView.Adapter<TournamentAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardTournamentBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val tournament = tournaments[holder.adapterPosition]
-        holder.bind(tournament)
-    }
-
-    override fun getItemCount(): Int = tournaments.size
-
-    class MainHolder(private val binding : CardTournamentBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(tournament: TournamentModel) {
-            binding.tournamentTitle.text = tournament.title
-            binding.tournamentOrg.text = "Organiser: ${tournament.org}"
-            binding.tournamentStartDate.text = "Start Date: ${tournament.startDate}"
-            binding.tournamentMaxTeams.text = "Max Teams: ${tournament.maxTeams}"
-        }
-    }
 }
